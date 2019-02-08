de2278c2298998da04d2ff2ba63d8a151a8d948d
liyuandan <lilylcn13@gmail.com>
2019-02-08 15:29:28 -0500

    added coopAdminServiceTest, test for create CoopAdmin

---
+++ Backend/src/test/java/ca/mcgill/ecse321/cooperator/service/CoopAdminServiceTest.java
@@ -0,0 +1,55 @@
+package ca.mcgill.ecse321.cooperator.service;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import ca.mcgill.ecse321.cooperator.entity.CoopAdmin;
+import ca.mcgill.ecse321.cooperator.repository.CoopAdminRepository;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CoopAdminServiceTest {
+	@Autowired
+	private CoopAdminService coopAdminService;
+	@Autowired
+	private CoopAdminRepository coopAdminRepository;
+	
+	@After
+	public void clearDatabase() {	
+		coopAdminRepository.deleteAll();
+	}
+	
+	@Test
+	public void testCreateCoopAdmin() {
+		assertEquals(0, coopAdminService.getAllCoopAdmins().size());
+
+		String email = "test@gmail.com";
+		String password = "12345678";
+		String name = "testName";
+
+		CoopAdmin coopAdmin = null;
+		try {
+			coopAdmin = coopAdminService.createCoopAdmin(email, password, name);
+		} catch (IllegalArgumentException e) {
+			// Check that no error occurred
+			fail();
+		}
+
+		List<CoopAdmin> allCoopAdmins = coopAdminService.getAllCoopAdmins();
+
+		assertEquals(1, allCoopAdmins.size());
+		
+		assertEquals(name, coopAdmin.getName());
+		assertEquals(email, coopAdmin.getEmail());
+		assertEquals(password, coopAdmin.getPassword());
+	}
+}
