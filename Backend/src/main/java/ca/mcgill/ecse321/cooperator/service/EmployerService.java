5a6eb959d3af691e4e51039870299f854aee4a3b
FulinHuang <fulin.huang@mail.mcgill.ca>
2019-02-09 13:36:38 -0500

    employer files

--- Backend/src/main/java/ca/mcgill/ecse321/cooperator/service/EmployerService.java
+++ Backend/src/main/java/ca/mcgill/ecse321/cooperator/service/EmployerService.java
@@ -3,27 +3,19 @@ package ca.mcgill.ecse321.cooperator.service;
 
 import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
-
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
-import org.springframework.test.context.junit4.SpringRunner;
-
-import java.util.ArrayList;
 import java.util.List;
 import ca.mcgill.ecse321.cooperator.entity.Employer;
 import ca.mcgill.ecse321.cooperator.repository.EmployerRepository;
-import org.springframework.data.repository.CrudRepository;
 
 
 
 @Service
 public class EmployerService {
 	
-	@Autowired
-	EntityManager entityManager;
+	//@Autowired
+	//EntityManager entityManager;
 	
 	@Autowired
 	EmployerRepository employerRepository;
@@ -31,13 +23,29 @@ public class EmployerService {
 	@Transactional
 	public Employer createEmployer(String userEmail, String userPassword, String companyName) {
 		Employer e = new Employer(userEmail, userPassword, companyName);
-		entityManager.persist(e);
-		return e;
+	//	entityManager.persist(e);
+		
+		if (userEmail == null || userEmail.trim().length() == 0) {
+			throw new IllegalArgumentException("Email cannot be empty!");
+		}
+		if (userPassword == null || userPassword.trim().length() == 0) {
+			throw new IllegalArgumentException("password cannot be empty!");
+		}
+	    if (companyName == null || companyName.trim().length() ==0) {
+			throw new IllegalArgumentException("company name cannot be empty!");
+		}
+	    
+	    e.setEmail(userEmail);
+		e.setPassword(userPassword);
+		e.setName(companyName);
+		Employer eReturn = employerRepository.save(e);
+		
+		return eReturn;
 	}
 	
 	@Transactional
 	public Employer getEmployer(int employerId) {
-		Employer e = entityManager.find(Employer.class, employerId);
+		Employer e = employerRepository.findById(employerId).get();
 		return e;
 	}
 	
@@ -45,7 +53,5 @@ public class EmployerService {
 	public List<Employer> getAllEmployers() {
 		return (List<Employer>) employerRepository.findAll();
 	}
-
-	
 }
 
--- Backend/src/test/java/ca/mcgill/ecse321/cooperator/service/EmployerPersistenceTest.java
+++ Backend/src/test/java/ca/mcgill/ecse321/cooperator/service/EmployerPersistenceTest.java
@@ -1,4 +1,4 @@
-package ca.mcgill.ecse321.persistenceTest;
+package ca.mcgill.ecse321.cooperator.service;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
@@ -49,13 +49,13 @@ public class EmployerPersistenceTest {
 			}
 
 			List<Employer> allEmployers = service.getAllEmployers();
-			Employer employer = null; //??/
+			Employer employer = null;
 
 			assertEquals(1, allEmployers.size());
 			
 			assertEquals(userEmail, employer.getEmail());
 			assertEquals(userPassword, employer.getPassword());
-			assertEquals(companyName, employer.getCompanyName());
+			assertEquals(companyName, employer.getName());
 			
 		}
 
