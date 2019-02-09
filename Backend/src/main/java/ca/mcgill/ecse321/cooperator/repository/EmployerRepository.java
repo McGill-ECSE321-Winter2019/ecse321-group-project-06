c71c89d5355f494a8884db385846f761aaa5808e
FulinHuang <fulin.huang@mail.mcgill.ca>
2019-02-09 16:12:03 -0500

    modify employerRepository class

--- Backend/src/main/java/ca/mcgill/ecse321/cooperator/repository/EmployerRepository.java
+++ Backend/src/main/java/ca/mcgill/ecse321/cooperator/repository/EmployerRepository.java
@@ -1,5 +1,6 @@
 package ca.mcgill.ecse321.cooperator.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import ca.mcgill.ecse321.cooperator.entity.Employer;
@@ -11,5 +12,8 @@ public interface EmployerRepository extends CrudRepository<Employer, Integer> {
 	
 	public Employer getEmployer(int employerId);
 
+	@Query("select u from User u where u.emailAddress = ?1")
+	  Employer findByEmailAddress(String emailAddress);
+	
 
 }
