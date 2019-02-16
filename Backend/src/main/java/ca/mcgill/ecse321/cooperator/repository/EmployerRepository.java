a48b069d54d8d8303a46f82beb7f9591e812df9a
liyuandan <lilylcn13@gmail.com>
2019-02-16 16:36:07 -0500

    employer repository dao implementation

--- Backend/src/main/java/ca/mcgill/ecse321/cooperator/repository/EmployerRepository.java
+++ Backend/src/main/java/ca/mcgill/ecse321/cooperator/repository/EmployerRepository.java
@@ -7,10 +7,6 @@ import ca.mcgill.ecse321.cooperator.entity.Employer;
 
 public interface EmployerRepository extends CrudRepository<Employer, Integer> {
 	
-	//public Employer createEmployer(String userEmail, String userPassword, String companyName, int employerId);
-	
-	//public Employer getEmployer(int employerId);
-	
-	
+	Employer findByName(String name);
 
 }
