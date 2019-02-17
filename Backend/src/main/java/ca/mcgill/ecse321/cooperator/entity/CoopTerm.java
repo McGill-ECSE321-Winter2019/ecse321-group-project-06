ad28779ab6c3b879dc466622233fbfaea7609868
liyuandan <lilylcn13@gmail.com>
2019-02-17 14:39:16 -0500

    removed constructor

--- Backend/src/main/java/ca/mcgill/ecse321/cooperator/entity/CoopTerm.java
+++ Backend/src/main/java/ca/mcgill/ecse321/cooperator/entity/CoopTerm.java
@@ -110,19 +110,4 @@ public class CoopTerm{
 	public CoopTerm() {
 		
 	}
-		
-
-//	public CoopTerm(String location, Date startDate, String academicSemester, boolean ifWorkPermitNeeded,
-//			String jobDescription, Student student, Employer employer, CoopAdmin coopAdmin, Date endDate) {
-//		this.location = location;
-//		this.startDate = startDate;
-//		this.endDate = endDate;
-//		this.academicSemester = academicSemester;
-//		this.ifWorkPermitNeeded = ifWorkPermitNeeded;
-//		this.jobDescription = jobDescription;
-//		this.student = student;
-//		this.employer = employer;
-//		this.coopAdmin = coopAdmin;
-//	}
-
 }
