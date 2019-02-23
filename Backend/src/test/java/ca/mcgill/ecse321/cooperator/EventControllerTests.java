a481076608110130a5326f5c151f581671a39f27
liyuandan <lilylcn13@gmail.com>
2019-02-23 16:53:57 -0500

    updated tests

--- Backend/src/test/java/ca/mcgill/ecse321/cooperator/EventControllerTests.java
+++ Backend/src/test/java/ca/mcgill/ecse321/cooperator/EventControllerTests.java
@@ -5,6 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import ca.mcgill.ecse321.cooperator.controller.EventController;
+import ca.mcgill.ecse321.cooperator.entity.Employer;
 import ca.mcgill.ecse321.cooperator.entity.Event;
 import ca.mcgill.ecse321.cooperator.repository.EventRepository;
 import ca.mcgill.ecse321.cooperator.service.EventService;
@@ -21,6 +22,11 @@ import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import java.sql.Time;
+import java.time.LocalTime;
+import java.util.Calendar;
+import java.util.Date;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class EventControllerTests {
@@ -35,4 +41,28 @@ public class EventControllerTests {
 	
 	private Event event;
 	
+	@Before
+	public void setupMock() {
+		Calendar c = Calendar.getInstance();
+		String location = "location";
+		c.set(2016, Calendar.OCTOBER, 16, 9, 00, 0);
+ 		Date startdate = new Date(c.getTimeInMillis());
+		LocalTime starttime = LocalTime.parse("09:00");
+		c.set(2016, Calendar.OCTOBER, 16, 9, 30, 0);
+		Date enddate = new Date(c.getTimeInMillis());
+		LocalTime endtime = LocalTime.parse("09:30");
+		
+		event = new Event();
+		event.setEndDate(enddate);
+		event.setStartDate(startdate);
+		event.setEventId(1);
+		event.setLocation("Montreal");
+		event.setStartTime(Time.valueOf(starttime));
+		event.setEndTime(Time.valueOf(endtime));
+	}
+
+	@Test
+	public void testMockPersonCreation() {
+		assertNotNull(event);
+	}
 }
