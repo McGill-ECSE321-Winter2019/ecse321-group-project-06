60e70f3fd58a968c019ddcb08e2e834453191320
FulinHuang <fulin.huang@mail.mcgill.ca>
2019-02-26 15:02:42 -0500

    add methods in repository classes

--- Backend/src/main/java/ca/mcgill/ecse321/cooperator/repository/EmployerRepository.java
+++ Backend/src/main/java/ca/mcgill/ecse321/cooperator/repository/EmployerRepository.java
@@ -6,4 +6,5 @@ import ca.mcgill.ecse321.cooperator.entity.Employer;
 
 public interface EmployerRepository extends CrudRepository<Employer, Integer> {
 	Employer findByName(String name);
+	Employer findById(int id);
 }
