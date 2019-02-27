5c4a9c542bf4f650ed1e385c1445c53eae28a997
FulinHuang <fulin.huang@mail.mcgill.ca>
2019-02-26 19:28:43 -0500

    Added test method in studentControllerTests class. Tests are related to #12 REQ3 and #13 REQ4

--- Backend/src/test/java/ca/mcgill/ecse321/cooperator/StudentControllerTests.java
+++ Backend/src/test/java/ca/mcgill/ecse321/cooperator/StudentControllerTests.java
@@ -5,7 +5,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import ca.mcgill.ecse321.cooperator.controller.StudentController;
-import ca.mcgill.ecse321.cooperator.entity.Employer;
 import ca.mcgill.ecse321.cooperator.entity.Student;
 import ca.mcgill.ecse321.cooperator.repository.StudentRepository;
 import ca.mcgill.ecse321.cooperator.service.StudentService;
@@ -23,6 +22,8 @@ import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import java.util.List;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -37,8 +38,12 @@ public class StudentControllerTests {
 	private StudentController studentController;
 	
 	private Student student;
+	private Student student1;
+	private List<Student> studentList;
 	private static final String STUDENT_KEY = "TestStudent";
-	private static final String NONEXISTING_KEY = "NotAStudent";
+	private static final String STUDENT1_KEY = "TestStudent1";
+	
+	
 	
 	@Before
 	public void setupMock() {
@@ -47,20 +52,47 @@ public class StudentControllerTests {
 		student.setEmail("testStudent@mail.mcgill.ca");
 		student.setName(STUDENT_KEY);
 		student.setPassword("test password");
+		
+		student1 = new Student();
+		student1.setCoopUserId(2);
+		student1.setEmail("testStudent1@mail.mcgill.ca");
+		student1.setName(STUDENT1_KEY);
+		student1.setPassword("test password1");
+		
+		studentList.add(student);
+		studentList.add(student1);
 	}
 	
 	@Test
 	public void testMockPersonCreation() {
 		assertNotNull(student);
+		assertNotNull(student1);
 	}
 	
 	@Test
 	public void testParticipantQueryFound() {
-		when(studentService.getStudent(anyInt())).thenReturn(student);
+		when(studentDao.findById(anyInt())).thenAnswer( (InvocationOnMock invocation) -> {
+		     return student;
+		});
 		assertEquals(STUDENT_KEY, studentService.getStudent(1).getName());
 		assertEquals("testStudent@mail.mcgill.ca", studentService.getStudent(1).getEmail());
 		assertEquals("test password", studentService.getStudent(1).getPassword());
 		
+	}
+	
+	/**/
+	@Test
+	public void testAllParticipantQueryFound() {
+		when(studentService.getAllStudents()).thenAnswer((InvocationOnMock invocation) -> {
+			return studentList;
+		});
+		assertEquals(STUDENT_KEY, studentService.getAllStudents().get(0).getName());
+		assertEquals("testStudent1@mail.mcgill.ca", studentService.getAllStudents().get(0).getEmail());
+		assertEquals("test password", studentService.getAllStudents().get(0).getPassword());
+		
+		assertEquals(STUDENT1_KEY, studentService.getAllStudents().get(1).getName());
+		assertEquals("testStudent1@mail.mcgill.ca", studentService.getAllStudents().get(1).getEmail());
+		assertEquals("test password1", studentService.getAllStudents().get(1).getPassword());	
 	}
 }
 	
