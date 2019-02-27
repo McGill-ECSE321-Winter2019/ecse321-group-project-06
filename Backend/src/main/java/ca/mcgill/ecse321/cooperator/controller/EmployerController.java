a892ce9b2227ae9c63d4e6fc5fe1f7d38ca8506b
liyuandan <lilylcn13@gmail.com>
2019-02-27 15:44:03 -0500

    update

--- Backend/src/main/java/ca/mcgill/ecse321/cooperator/controller/EmployerController.java
+++ Backend/src/main/java/ca/mcgill/ecse321/cooperator/controller/EmployerController.java
@@ -1,10 +1,18 @@
 package ca.mcgill.ecse321.cooperator.controller;
 
+import java.util.Set;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import ca.mcgill.ecse321.cooperator.dto.EmployerDto;
 
 @CrossOrigin(origins = "*")
 @RestController
+@RequestMapping("/employer")
 public class EmployerController {
-
+	
 }
