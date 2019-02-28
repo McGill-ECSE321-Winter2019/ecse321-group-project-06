dac30af8050897acd19ae23aa9b29d1dc2b54086
liyuandan <lilylcn13@gmail.com>
2019-02-27 21:09:28 -0500

    removed unnecessary import

--- Backend/src/main/java/ca/mcgill/ecse321/cooperator/controller/EmployerController.java
+++ Backend/src/main/java/ca/mcgill/ecse321/cooperator/controller/EmployerController.java
@@ -1,18 +1,9 @@
 package ca.mcgill.ecse321.cooperator.controller;
 
-import java.util.Set;
-
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import ca.mcgill.ecse321.cooperator.dto.EmployerDto;
 
 @CrossOrigin(origins = "*")
 @RestController
-@RequestMapping("/employer")
-public class EmployerController {
-	
+public class EmployerController {	
 }
