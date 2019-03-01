155c416a938546cd62745a4e094413466a223377
zhuzhenLi <lizhuzhen8760@gmail.com>
2019-03-01 09:56:41 -0500

    fix wrong import

--- Backend/src/test/java/ca/mcgill/ecse321/cooperator/service/EmployerServiceTest.java
+++ Backend/src/test/java/ca/mcgill/ecse321/cooperator/service/EmployerServiceTest.java
@@ -6,7 +6,7 @@ import static org.junit.Assert.fail;
 import java.util.List;
 
 import org.junit.After;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
