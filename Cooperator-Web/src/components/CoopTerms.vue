6899a5a9d43e6d2ba23114fe46e3731a74444bd4
chuningli <lilylcn13@gmail.com>
2019-03-18 22:48:24 -0400

    created default layout

---
+++ Cooperator-Web/src/components/CoopTerms.vue
@@ -0,0 +1,13 @@
+<template>
+    
+</template>
+
+<script>
+    export default {
+        name: "CoopTerms.vue"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ Cooperator-Web/src/components/NavItem.vue
@@ -0,0 +1,13 @@
+<template>
+    
+</template>
+
+<script>
+    export default {
+        name: "NavItem"
+    }
+</script>
+
+<style scoped>
+
+</style>
