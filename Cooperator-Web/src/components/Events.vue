8ef7b18e8a9ba7c17ccc53a17e76326895ecfc36
chuningli <lilylcn13@gmail.com>
2019-03-19 16:10:38 -0400

    deployed frontend app to heroku

--- Cooperator-Web/src/components/Events.vue
+++ Cooperator-Web/src/components/Events.vue
@@ -1,7 +1,8 @@
 <template>
-  <div id="events">
+  <div>
     <h1>Events</h1>
-      <b-row>
+    <b-container id="events">
+      <b-row class="no-gutters">
         <b-col>
           <Event event="Tech Fair" text="Whether you have jobs today or will have jobs in the future, McGill students and alumni want to know more about your organization and potential careers!"/>
         </b-col>
@@ -9,6 +10,7 @@
           <Event event="On Campus Recruitment" text="on campus recruitment for your organization"/>
         </b-col>
       </b-row>
+    </b-container>
   </div>
 </template>
 
@@ -23,5 +25,7 @@
 </script>
 
 <style scoped>
-
+#events{
+  padding: 25px;
+}
 </style>
