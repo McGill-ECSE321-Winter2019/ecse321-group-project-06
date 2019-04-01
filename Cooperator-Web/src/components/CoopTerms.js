53a8d7126cdd306e91f2b3acbf8a0302fbdae5a7
zhuzhenLi <lizhuzhen8760@gmail.com>
2019-04-01 15:41:27 -0400

    frontend get all coopterms and students name

--- Cooperator-Web/src/components/CoopTerms.js
+++ Cooperator-Web/src/components/CoopTerms.js
@@ -2,8 +2,11 @@ import axios from 'axios'
 var config = require('../../config')
 
 
+//var frontendUrl = 'http://' + config.dev.host + ':' + config.dev.port
+//var backendUrl='https://cooperator-backend-060606.herokuapp.com/'
+
 var frontendUrl = 'http://' + config.dev.host + ':' + config.dev.port
-var backendUrl='https://cooperator-backend-060606.herokuapp.com/'
+var backendUrl = 'http://' + config.dev.backendHost + ':' + config.dev.backendPort
 
 var AXIOS = axios.create({
   baseURL: backendUrl,
@@ -11,72 +14,69 @@ var AXIOS = axios.create({
 })
 
 export default {
-  name:"CoopTerms",
     data(){
       return {
-        template:[
-          {
-            name:'huanhuan',
-            startDate: '2019',
-            endDate: '2021',
-            active: 'true'
-          },
-          {
-            name:'yingying',
-            startDate: '2019',
-            endDate: '2020',
-            active: 'true'
-          },
-          {
-            name: 'xixi',
-            startDate: '2019',
-            endDate: '2023',
-            active: 'false'
-          },
-          {
-            name:'beibei',
-            startDate: '2019',
-            endDate: '2020',
-            active: 'false'
-          },
-          {
-            name:'jingjing',
-            startDate: '2019',
-            endDate: '2039',
-            active: 'false'
-          },
-          {
-            name:'nini',
-            startDate: '2019',
-            endDate: '2022',
-            active: 'false'
-          }
-        ],
-        coopTerms:[],
-        error:'',
+
+        students: '',
+        studentsName: [],
+        coopTerms:[]
       }
   },
-  create: function () {
-    AXIOS.get(`/coopTerms`)
-      .then(response => {
-        this.coopTerms = response.data
+
+    created: function () {
+      AXIOS.get(`coopTerm/employer/4`)
+       .then(response => {
+         this.coopTerms = response.data
+
       })
-      .catch(e =>{
-        console.log("error in post request: " + e);
-        this.error = e;
-      })
+        .catch(e =>{
+         console.log("error in get coopterm request: " + e);
+         this.error = e;
+        })
   },
 
-  methods:{
-    isActive: function(coopTerm){
-      var i;
-      if(coopTerm.active==="true"){
+  methods: {
+    isActive: function (coopTerm) {
+
+      if (coopTerm.state === "ACTIVE") {
         return "Active";
-      }else{
+      } else {
         return "Not Active";
       }
+    },
+
+    studentName: function (id,index) {
+
+      AXIOS.get(`student/` + id)
+        .then(response =>{
+          this.students = response.data;
+          this.studentsName.push(this.students.name) ;
+        })
+        .catch(e => {
+                    console.log("error in get student request:" + e);
+                    this.error = e;
+                  })
+
+      return  this.studentsName[index];
+
+      // .then(()=> {
+      //     for (var i = 0; i < this.coopTerms.length; i++) {
+      //       var id = this.coopTerms[i].studentId
+      //
+      //       AXIOS.get(`student/` + id)
+      //         .then(res => {
+      //           this.coopTerms[i].student = res.data
+      //           this.coopTerms[i].studentName=this.coopTerms[i].student.name
+      //         })
+      //         .catch(e => {
+      //           console.log("error in get student request:" + e);
+      //           this.error = e;
+      //         })
+      //     }
+      //   })
+
     }
 
-
   }
+
 }
--- Cooperator-Web/src/components/CoopTerms.vue
+++ Cooperator-Web/src/components/CoopTerms.vue
@@ -7,25 +7,29 @@
             <div class="container" align="center">
 
 
-              <table style="width: 100%; height: 100%;" id="CoopTermTable">
-                <tr style="font-size:25px" >
-                  <th style="padding:10px" id = "text">NAME</th>
-                  <th style="padding:10px" id = "text">START DATE</th>
-                  <th style="padding:10px" id = "text">END DATE</th>
-                  <th style="padding:10px" id = "text">STATUS</th>
-                  <th style="padding:10px" id = "text">ACTION</th>
+              <table style="width: 100%; height: 100%; border:1px solid black; border-collapse:collapse;"  id="CoopTermTable" >
+                <tr style="font-size:25px"    >
+                  <th style="padding:10px; border:1px solid black;"   id = "text">NAME</th>
+                  <th style="padding:10px; border:1px solid black;" id = "text">START DATE</th>
+                  <th style="padding:10px; border:1px solid black;" id = "text">END DATE</th>
+                  <th style="padding:10px; border:1px solid black;" id = "text">STATUS</th>
+                  <th style="padding:10px; border:1px solid black;" id = "text">ACTION</th>
                 </tr>
-                <tr v-for="coopTerm in template">
-                  <td style="padding:10px" id = "contentText">{{coopTerm.name}}</td>
-                  <td style="padding:10px" id = "contentText">{{coopTerm.startDate}}</td>
-                  <td style="padding:10px" id = "contentText">{{coopTerm.endDate}}</td>
-                  <td style="padding:10px" id = "contentText">{{isActive(coopTerm)}}</td>
-                  <td>
+
+                <tr v-for="coopTerm,index in coopTerms" >
+                  <td style="padding:10px; border:1px solid black;" id = "contentText">{{studentName(coopTerm.studentId,index)}}</td>
+                  <td style="padding:10px; border:1px solid black;" id = "contentText">{{coopTerm.startDate.substr(0,10)}}</td>
+                  <td style="padding:10px; border:1px solid black;" id = "contentText">{{coopTerm.endDate.substr(0,10)}}</td>
+                  <td style="padding:10px; border:1px solid black;" id = "contentText">{{coopTerm.state}}</td>
+                  <td style="padding:10px; border:1px solid black;">
                     <b-button id = "button"   v-bind:to="{path:'/Coop-Page'}" class="mr-1" style=" margin: 10px" >      view   </b-button>
 
                   </td>
                   <td></td>
                 </tr>
+
+
+
               </table>
 
 
@@ -44,23 +48,27 @@
   padding: 10px;
 }
   #CoopTermTable{
+   // border: 1px solid black;
     background-color: #D2D1D1;
-    border: 20px;
     margin: 5px;
     font-size: 20px;
+
   }
 
   #text{
+   // border: 1px solid black;
     text-align: center;
     color: #EC3A0E;
   }
 
   #contentText{
+   // border: 1px solid black;
     text-align: center;
     color: #0C0A0A;
   }
 
   #button{
+
     size:lg;
     margin:auto;
     display:block;
