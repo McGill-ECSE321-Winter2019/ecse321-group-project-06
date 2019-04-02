e0ccb72211320d4d1993e0bfb9b6c7def9fef568
XinquanWang <xinquan.wang@mail.mcgill.ca>
2019-04-01 21:57:41 -0400

    update login backend

--- Backend/src/main/java/ca/mcgill/ecse321/cooperator/controller/EmployerController.java
+++ Backend/src/main/java/ca/mcgill/ecse321/cooperator/controller/EmployerController.java
@@ -80,8 +80,8 @@ public class EmployerController {
 		List<EmployerDto> employers = new ArrayList<>();
 		for (Employer employer : service.getAllEmployers()) {
 
-			if(employer.getEmail() == email) {
-				if(employer.getPassword() == password) {
+			if(employer.getEmail().equals(email)) {
+				if(employer.getPassword().equals(password)) {
 					// valid password , add to list 
 					employers.add(convertToDto(employer));
 				}else {
