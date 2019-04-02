95c92d6b33356ee879fde27ae89b620b604b5e62
chuningli <lilylcn13@gmail.com>
2019-04-01 22:29:01 -0400

    url changed

--- Cooperator-Web/src/components/Register.js
+++ Cooperator-Web/src/components/Register.js
@@ -4,8 +4,8 @@ var config = require('../../config')
 // var frontendUrl = 'http://' + config.dev.host + ':' + config.dev.port
 // var backendUrl = 'http://' + config.dev.backendHost + ':' + config.dev.backendPort
 
-var frontendUrl = 'http://' + config.build.host + ':'
-var backendUrl = 'http://' + config.build.backendHost + ':'
+var frontendUrl = 'https://' + config.build.host + ':'
+var backendUrl = 'https://' + config.build.backendHost + ':'
 
 var AXIOS = axios.create({
   baseURL: backendUrl,
--- Cooperator-Web/src/components/events.js
+++ Cooperator-Web/src/components/events.js
@@ -7,8 +7,8 @@ import Event from './Event'
 // var backendUrl='https://cooperator-backend-060606.herokuapp.com/'
 // var backendUrl = 'http://' + config.dev.backendHost + ':' + config.dev.backendPort
 
-var frontendUrl = 'http://' + config.build.host + ':'
-var backendUrl = 'http://' + config.build.backendHost + ':'
+var frontendUrl = 'https://' + config.build.host + ':'
+var backendUrl = 'https://' + config.build.backendHost + ':'
 
 var AXIOS = axios.create({
   baseURL: backendUrl,
