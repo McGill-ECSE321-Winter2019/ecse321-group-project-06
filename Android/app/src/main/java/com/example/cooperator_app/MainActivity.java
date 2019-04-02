c33c0f6ce1f315cecb8080969b64775bdf0af111
tyclany <yin.zhang@mail.mcgill.ca>
2019-04-02 17:18:24 -0400

    login implemented

---
+++ Android/app/src/main/java/com/example/cooperator_app/MainActivity.java
@@ -0,0 +1,71 @@
+package com.example.cooperator_app;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import cz.msebera.android.httpclient.Header;
+
+public class MainActivity extends AppCompatActivity {
+    private String error = "";
+    private EditText localEmailet;
+    private EditText localPassword;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        localEmailet = (EditText) findViewById(R.id.Email);
+        localPassword = (EditText) findViewById(R.id.Password);
+    }
+
+    public void login(View v){
+        String email = localEmailet.getText().toString();
+        String password = localPassword.getText().toString();
+        RequestParams rp = new RequestParams();
+        rp.add("Email",email);
+        rp.add("Password",password);
+        HttpUtils.get("/login", rp, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+//                refreshErrorMessage();
+//                tv.setText("");
+                Intent intent= new Intent(MainActivity.this, dashboard.class);
+                startActivity(intent);
+            }
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                try {
+                    error += errorResponse.get("message").toString();
+                } catch (JSONException e) {
+                    error += e.getMessage();
+                }
+                refreshErrorMessage();
+            }
+        });
+    }
+    private void refreshErrorMessage() {
+        // set the error message
+        TextView tvError =findViewById(R.id.error);
+        tvError.setText(error);
+
+        if (error == null || error.length() == 0) {
+            tvError.setVisibility(View.GONE);
+        } else {
+            tvError.setVisibility(View.VISIBLE);
+        }
+    }
+
+}
---
+++ Android/app/src/main/java/com/example/cooperator_app/dashboard.java
@@ -0,0 +1,13 @@
+package com.example.cooperator_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class dashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+    }
+}
