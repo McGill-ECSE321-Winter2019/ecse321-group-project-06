a8ad6a18388315f8b5272e17feff6e6c07bf9a85
tyclany <yin.zhang@mail.mcgill.ca>
2019-04-07 23:30:27 -0400

    receive coopUserId to diplay coopTerm from database properly

--- Android/app/src/main/java/com/example/cooperator_app/viewCoopterm.java
+++ Android/app/src/main/java/com/example/cooperator_app/viewCoopterm.java
@@ -1,25 +1,17 @@
 package com.example.cooperator_app;
 
-import android.annotation.TargetApi;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ListView;
 import android.widget.TextView;
-import java.util.HashMap;
-
-
 import com.loopj.android.http.JsonHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
-
 import cz.msebera.android.httpclient.Header;
-
 import static android.util.Log.e;
 
 public class viewCoopterm extends AppCompatActivity {
@@ -28,13 +20,15 @@ public class viewCoopterm extends AppCompatActivity {
     JSONArray employerArray;
     CoopTermAdapter coopTermAdapter;
     ListView listview;
+    String coopUserId = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        HttpUtils.get("/coopTerm/employer/1", new RequestParams(), new JsonHttpResponseHandler() {
+        coopUserId=this.getIntent().getStringExtra("coopUserId");
+        HttpUtils.get("/coopTerm/employer/"+coopUserId, new RequestParams(), new JsonHttpResponseHandler() {
             @Override
-            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+            public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
                 json_string = response.toString();
                 try {
                     employerArray = new JSONArray(json_string);
@@ -42,13 +36,12 @@ public class viewCoopterm extends AppCompatActivity {
                     String startDate, endDate, state;
                     while (count < employerArray.length()) {
                         JSONObject e = employerArray.getJSONObject(count);
-                        startDate = e.getString("stratDate");
+                        startDate = e.getString("startDate");
                         endDate = e.getString("endDate");
                         state = e.getString("state");
                         CoopTerm coopTerm = new CoopTerm(startDate, endDate, state);
                         coopTermAdapter.add(coopTerm);
                         count++;
-
                     }
                 } catch (JSONException e) {
                     e("MYAPP", "unexpected JSON exception", e);
@@ -72,6 +65,7 @@ public class viewCoopterm extends AppCompatActivity {
 
     public void directToHome(View v){
         Intent intent= new Intent(this, dashboard.class);
+        intent.putExtra("coopUserId",coopUserId);
         startActivity(intent);
     }
 
