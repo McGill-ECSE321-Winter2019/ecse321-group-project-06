3ab62d06a457ae9ce88bc805865d31848e31a1b4
chuningli <lilylcn13@gmail.com>
2019-04-07 23:27:55 -0400

    code cleanup

--- Backend/src/main/java/ca/mcgill/ecse321/cooperator/repository/CoopTermRepository.java
+++ Backend/src/main/java/ca/mcgill/ecse321/cooperator/repository/CoopTermRepository.java
@@ -8,9 +8,38 @@ import ca.mcgill.ecse321.cooperator.entity.CoopTerm;
 
 
 public interface CoopTermRepository extends CrudRepository<CoopTerm, Integer> {
+	/**
+	 * get a coopterm by startDate
+	 * @param startDate
+	 * @return CoopTerm
+	 */
 	CoopTerm findByStartDate(Date startDate);
+	/**
+	 * get coopterm by endDate
+	 * 
+	 * @param endDate
+	 * @return CoopTerm
+	 */
 	CoopTerm findByEndDate(Date endDate);
+	/**
+	 * get a coopterm by location
+	 * 
+	 * @param location
+	 * @return
+	 */
 	CoopTerm findByLocation(String location);
+	/**
+	 * get a coopterm by academic semester
+	 * 
+	 * @param academicSemester
+	 * @return CoopTerm
+	 */
 	CoopTerm findByAcademicSemester(String academicSemester);
+	/**
+	 * get a coopterm by id
+	 * 
+	 * @param id
+	 * @return CoopTerm
+	 */
 	CoopTerm findById(int id);
 }
--- Backend/src/main/java/ca/mcgill/ecse321/cooperator/repository/EventRepository.java
+++ Backend/src/main/java/ca/mcgill/ecse321/cooperator/repository/EventRepository.java
@@ -7,9 +7,39 @@ import org.springframework.data.repository.CrudRepository;
 import ca.mcgill.ecse321.cooperator.entity.Event;
 
  public interface EventRepository extends CrudRepository<Event, Integer>{
+	 /**
+	  * find Event by start date
+	  * 
+	  * @param startDate
+	  * @return Event
+	  */
 	 Event findByStartDate(Date startDate);
+	 
+	 /**
+	  * get an event by end date
+	  * @param endDate
+	  * @return Event
+	  */
 	 Event findByEndDate(Date endDate);
+	 
+	 /**
+	  * get an event by the location
+	  * 
+	  * @param location
+	  * @return Event
+	  */
 	 Event findByLocation(String location);
+	 
+	 /**
+	  * get an event by id
+	  * @param id
+	  * @return
+	  */
 	 Event findById(int id);
+	 
+	 /**
+	  * delete an event by id
+	  * @param id
+	  */
 	 void deleteById(int id);
  }
